5b87b9e3583cff184fd4e32f8fd1c4d4679a011c
Caijinglong <cjl_spy@163.com>
2019-12-13 09:21:06 +0800

    device的代码结构

---
+++ android/src/main/kotlin/top/kikt/bt/ble/bluetooth_ble/core/DeviceManager.kt
@@ -0,0 +1,5 @@
+package top.kikt.bt.ble.bluetooth_ble.core
+
+/// create 2019-12-13 by cai
+
+
--- android/src/main/kotlin/top/kikt/bt/ble/bluetooth_ble/core/IBleDevice.kt
+++ android/src/main/kotlin/top/kikt/bt/ble/bluetooth_ble/core/IBleDevice.kt
@@ -1,28 +1,21 @@
 package top.kikt.bt.ble.bluetooth_ble.core
 
-import android.bluetooth.*
+import android.bluetooth.BluetoothDevice
 import android.os.Handler
 import android.os.Looper
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
-import top.kikt.bt.ble.bluetooth_ble.ext.checkProperty
-import top.kikt.bt.ble.bluetooth_ble.ext.toMap
-import top.kikt.bt.ble.bluetooth_ble.logger
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 /// create 2019-12-04 by cai
 
 
-abstract class IBleDevice(protected val registrar: PluginRegistry.Registrar, protected val device: BluetoothDevice, private val rssi: Int) : MethodChannel.MethodCallHandler, DelegateInvoke {
+abstract class IBleDevice(protected val registrar: PluginRegistry.Registrar, protected val device: BluetoothDevice, private val rssi: Int) : MethodChannel.MethodCallHandler {
   
   val id: String = device.address
   
   val notifyingMap = HashMap<String, Boolean>()
   
-  override val channel = MethodChannel(registrar.messenger(), "top.kikt/ble/$id")
+  private val channel = MethodChannel(registrar.messenger(), "top.kikt/ble/$id")
   
   fun `init`() {
     channel.setMethodCallHandler(this)
@@ -51,179 +44,6 @@ abstract class IBleDevice(protected val registrar: PluginRegistry.Registrar, pro
       "rssi" to rssi
     )
   }
-  
-}
-
-class BleDevice(registrar: PluginRegistry.Registrar, device: BluetoothDevice, rssi: Int) : IBleDevice(registrar, device, rssi) {
-  
-  private var gatt: BluetoothGatt? = null
-  
-  private var services = ArrayList<BluetoothGattService>()
-  
-  inner class GattCallback : BluetoothGattCallback() {
-    override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
-      super.onConnectionStateChange(gatt, status, newState)
-      logger.debug("status = $status, newState = $newState")
-      when (newState) {
-        BluetoothGatt.STATE_CONNECTED -> {
-          notifyConnectState(gatt, true)
-        }
-        BluetoothGatt.STATE_CONNECTING -> {
-        }
-        BluetoothGatt.STATE_DISCONNECTED -> {
-          notifyingMap.clear()
-          notifyConnectState(gatt, false)
-        }
-        BluetoothGatt.STATE_DISCONNECTING -> {
-        }
-      }
-    }
-    
-    private fun notifyConnectState(gatt: BluetoothGatt?, isConnect: Boolean) {
-      if (isConnect) {
-        ConnectedBleManager.addDevice(this@BleDevice)
-        invokeMethod("onConnect")
-      } else {
-        ConnectedBleManager.removeDevice(this@BleDevice)
-        invokeMethod("onDisconnect")
-        gatt?.close()
-      }
-    }
-    
-    override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
-      super.onServicesDiscovered(gatt, status)
-      val services = gatt?.services
-      if (services != null) {
-        this@BleDevice.services.clear()
-        this@BleDevice.services.addAll(services)
-        val serviceResult = services.map { it.uuid.toString() }
-        discoverServiceHandler?.success(serviceResult)
-        discoverServiceHandler = null
-      } else {
-        discoverServiceHandler?.success(arrayListOf<String>())
-        discoverServiceHandler = null
-      }
-    }
-    
-    override fun onCharacteristicChanged(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?) {
-      super.onCharacteristicChanged(gatt, characteristic)
-      /// 接收消息
-      if (characteristic == null) {
-        return
-      }
-      invokeMethod("notifyValue", mapOf(
-        "serviceId" to characteristic.service.uuid.toString(),
-        "data" to characteristic.value,
-        "ch" to characteristic.toMap(true)
-      ))
-    }
-    
-    override fun onCharacteristicWrite(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?, status: Int) {
-      super.onCharacteristicWrite(gatt, characteristic, status)
-      logger.debug("onCharacteristicWrite: status=$status, value = ${characteristic?.value}")
-    }
-  }
-  
-  private val callback = GattCallback()
-  
-  var discoverServiceHandler: ReplyHandler? = null
-  
-  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-    val handler = ReplyHandler(call, result)
-    when (call.method) {
-      "connect" -> {
-        val type = handler.param<Int>("type") ?: BluetoothDevice.TRANSPORT_LE
-        this.gatt = device.connectGatt(registrar.activity(), false, callback, type)
-      }
-      "disconnect" -> {
-        gatt?.disconnect()
-      }
-      "discoverServices" -> {
-        discoverServiceHandler = handler
-        gatt?.discoverServices()
-      }
-      "discoverCharacteristics" -> {
-        val serviceId = call.argument<String>("service")!!
-        val service = gatt?.getService(UUID.fromString(serviceId))
-        val characteristicsList = service?.characteristics
-        val resultArray = ArrayList<Map<String, Any>>()
-        
-        if (characteristicsList != null) {
-          for (item in characteristicsList) {
-            val notifying = notifyingMap[item.uuid.toString()] ?: false
-            resultArray.add(item.toMap(notifying))
-          }
-        }
-        
-        handler.success(resultArray)
-      }
-      "changeNotify" -> {
-        val chId = call.argument<String>("ch")!!
-        val serviceId = call.argument<String>("service")!!
-        val notify = call.argument<Boolean>("notify")!!
-        
-        val service = gatt?.getService(UUID.fromString(serviceId))
-        val ch = service?.getCharacteristic(UUID.fromString(chId))
-        
-        if (ch != null) {
-          val notifyResult = setNotify(ch, notify)
-          if (notifyResult) {
-            notifyingMap[chId] = notify
-            invokeMethod("notifyState", mapOf(
-              "serviceId" to serviceId,
-              "ch" to ch.toMap(notify)
-            ))
-          }
-        }
-      }
-      "writeData" -> {
-        val chId = call.argument<String>("ch")!!
-        val serviceId = call.argument<String>("service")!!
-        val data = call.argument<ByteArray>("data")!!
-        
-        val service = gatt?.getService(UUID.fromString(serviceId)) ?: return
-        val ch = service.getCharacteristic(UUID.fromString(chId)) ?: return
-        
-        ch.value = data
-        gatt?.writeCharacteristic(ch)
-        handler.success(1)
-      }
-      else -> {
-      }
-    }
-  }
-  
-  // https://blog.csdn.net/ctlemon/article/details/82699144
-  private fun setNotify(ch: BluetoothGattCharacteristic, notify: Boolean): Boolean {
-    var result = gatt?.setCharacteristicNotification(ch, notify)
-    
-    if (result == true) {
-      for (dp in ch.descriptors) {
-        if (dp != null) {
-          if (ch.checkProperty(BluetoothGattCharacteristic.PROPERTY_NOTIFY)) {
-            if (notify) {
-              dp.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
-            } else {
-              dp.value = BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE
-            }
-          } else if (ch.checkProperty(BluetoothGattCharacteristic.PROPERTY_INDICATE)) {
-            if (notify) {
-              dp.value = BluetoothGattDescriptor.ENABLE_INDICATION_VALUE
-            } else {
-              dp.value = BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE
-            }
-          }
-          result = gatt?.writeDescriptor(dp)
-        }
-      }
-    }
-    return result ?: false
-  }
-  
-}
-
-private interface DelegateInvoke {
-  val channel: MethodChannel
   
   companion object {
     private val mainHandler = Handler(Looper.getMainLooper())
